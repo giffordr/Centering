c69475b8e1a6f65e54b4b98870c52476fc01baa4
Ray <giffordr@stanford.edu>
2022-05-22 10:52:06 -0700

    attempt fullscreen

--- components/game/Game.jsx
+++ components/game/Game.jsx
@@ -1,25 +1,47 @@
+'use strict';
 import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 class Game extends React.Component {
-  
-render(){
 
-  const unityContext = new UnityContext({
+unityContext = new UnityContext({
     loaderUrl: "./build/SDVisualizer_webGL_3.loader.js",
     dataUrl: "./build/SDVisualizer_webGL_3.data",
     frameworkUrl: "./build/SDVisualizer_webGL_3.framework.js",
     codeUrl: "./build/SDVisualizer_webGL_3.wasm",
     })
+
+componentDidMount() {
+    unityContext.setFullscreen(true);
+  }
+handleOnClickFullscreen() {
+    unityContext.setFullscreen(true);
+  }
   
-  return (<Unity
-      unityContext={unityContext}
+render(){
+
+  //const unityContext = new UnityContext({
+    //loaderUrl: "./build/SDVisualizer_webGL_3.loader.js",
+    //dataUrl: "./build/SDVisualizer_webGL_3.data",
+    //frameworkUrl: "./build/SDVisualizer_webGL_3.framework.js",
+    //codeUrl: "./build/SDVisualizer_webGL_3.wasm",
+   // })
+  
+  return (
+    <div>
+      
+    <Unity
+      unityContext={this.unityContext}
       style={{
         height: "100%",
         width: "100%",
         background: "grey",
       }}
-    />);
+    />
+    <button onClick={this.handleOnClickFullscreen}>Fullscreen</button>
+    </div>
+  
+  );
 }
 }
 
